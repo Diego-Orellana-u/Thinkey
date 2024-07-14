41fa2041facebcef33676a7d09e371a0f9c82ad1
diego orellana <diego.orellana.git@gmail.com>
2024-07-14 15:32:11 -0400

    added css reset

---
+++ src/data/nav.js
@@ -0,0 +1,36 @@
+export const nav = [
+  {
+    name: "Servicios",
+    link: "/servicios",
+    subnav: [
+      {
+        name: "Para familias",
+        link: "/familias",
+        key: "familiasnav",
+      },
+      {
+        name: "Para empresas",
+        link: "/empresas",
+        key: "empresasnav",
+      },
+      {
+        name: "Para organizaciones",
+        link: "/organizaciones",
+        key: "organizacionesnav",
+      },
+    ],
+    key: "serviciosnav",
+  },
+  {
+    name: "Nosotros",
+    link: "/nosotros",
+    subnav: [],
+    key: "nosotrosnav",
+  },
+  {
+    name: "Contacto",
+    link: "/contacto",
+    subnav: [],
+    key: "contactonav",
+  },
+];

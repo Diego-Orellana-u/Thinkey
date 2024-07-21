e2ba2c369f3ea3412e4fe47de8b0dd93af1aa8f2
diego orellana <diego.orellana.git@gmail.com>
2024-07-21 17:23:15 -0400

    added hero image, box shadow, texts

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
 import test from "../../../public/test.svg";
+import fotoHero from "../../../public/fotoHero.webp";
 
 export default function Hero() {
   return (
-    <div>
-      <div className="px-5 flex flex-col gap-5">
-        <span className="text-[28px] min-[345px]:text-[30px] leading-9 font-bold">
-          Frase Catchy sobre lo que hacemos
-        </span>
-        <p className="text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut.
-        </p>
+    <div className="pt-5">
+      <div className="px-5 flex flex-col gap-3">
+        <div>
+          <span className="text-sm">EVENTOS CIENTIFICOS Y EDUCATIVOS</span>
+        </div>
+        <div className="max-[600px]:block hidden drop-shadow-2xl">
+          <Image src={fotoHero} className="rounded-[20px] box-shadow-hero" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <span className="text-[28px] min-[345px]:text-[30px] leading-9 font-bold">
+            Frase Catchy sobre lo que hacemos
+          </span>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut.
+          </p>
+        </div>
       </div>
 
       <div>

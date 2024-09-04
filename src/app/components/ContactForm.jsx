7f756214a9cafc9baaefa29fa6d6d4558b962157
diego orellana <diego.orellana.git@gmail.com>
2024-09-04 02:06:54 -0400

    added new breakpoints for contact form

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -18,7 +18,7 @@ export default function ContactForm({ formInfo }) {
         formInfo.sectionMargin && formInfo.sectionMargin
       } flex flex-col bg-white items-center tablet-xl:flex-row tablet-xl:h-[900px] overflow-x-hidden`}
     >
-      <div className="wrapper border-b border-gray-300/30 relative pt-24 pb-20 w-full bg-main-200 h-full tablet-xl:flex-[0_0_50%] bg-no-repeat">
+      <div className="wrapper border-b border-gray-300/30 relative pt-24 tablet-l:pt-32 desktop-s:pt-52 desktop-l:pt-56 pb-20 tablet-l:pb-24 tablet-l:px-24 desktop-s:pl-[3.3rem] desktop-s:pr-10 desktop-l:px[5.75rem] w-full bg-main-200 h-full tablet-xl:flex-[0_0_50%] bg-no-repeat">
         <LineOne x="-right-24" y="top-0 z-10" opacity="opacity-20" />
         <Star
           fillColor="fill-accent-900"
@@ -29,35 +29,36 @@ export default function ContactForm({ formInfo }) {
           position="absolute"
         />
         <DibujoPocion />
-        <DibujoPlaneta />
         <DibujoMolecula />
         <span
-          className={`relative text-h2-s font-semibold leading-7 text-secondary-100 z-20`}
+          className={`relative text-h2-s tablet-l:text-h2-l font-semibold leading-7 text-secondary-100 z-20`}
         >
           Pongamonos en contacto
         </span>
-        <p className={`mt-6 leading-7 text-white-body-color`}>
+        <p className={`mt-6 text-p-l leading-8 text-white-body-color`}>
           Proin volutpat consequat porttitor cras nullam gravida at. Orci
           molestie a eu arcu. Sed ut tincidunt integer elementum id sem. Arcu
           sed malesuada et magna.
         </p>
-        <div className="flex flex-col gap-4 mt-8 text-white-body-color">
-          <div className="flex gap-3 items-center">
+        <div className="flex flex-col gap-4 mt-8 text-p-l leading-8 text-white-body-color">
+          <div className="flex gap-5 items-center">
             <BuildingIcon />
             <span>Almte. Zegers 672, Oficina C</span>
           </div>
-          <div className="flex gap-3 items-center">
+          <div className="flex gap-5 items-center">
             <PhoneIcon width="24" height="24" />
             <span>+1 (555) 234-5678</span>
           </div>
-          <div className="flex gap-3 items-center">
+          <div className="flex gap-5 items-center">
             <MailIcon />
-            <Link href="/">hello@example.com</Link>
+            <Link href="/" className="pb-[2px]">
+              hello@example.com
+            </Link>
           </div>
         </div>
       </div>
 
-      <div className="tablet-xl:pl-10 desktop-s:pl-12 desktop-l:pl-16 wrapper py-20 w-full">
+      <div className="tablet-xl:pl-10 tablet-l:px-[5.7rem] desktop-s:pl-12 desktop-l:pl-16 wrapper py-20 w-full h-full bg-white z-20">
         <ServicesForm
           formFields={formInfo.formFields}
           btnText={formInfo.btnText}

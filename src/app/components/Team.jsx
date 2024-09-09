d6cf2692faa253eb94de0d77f9d15ff2eca05203
diego orellana <diego.orellana.git@gmail.com>
2024-09-08 21:29:10 -0300

    added key props, modified duration, added dynamic data

--- src/app/components/Team.jsx
+++ src/app/components/Team.jsx
@@ -6,37 +6,40 @@ import CloseIcon from "./Icons/CloseIcon";
 import { useEffect, useState } from "react";
 
 export function TeamInfo({
-  openInfo,
-  setOpenInfo,
+  openSidebar,
+  setOpenSidebar,
+  sidebarInfo,
+  setSidebarInfo,
   overlayClicked,
   setOverlayClicked,
 }) {
   const handleOverlayClick = () => {
     setOverlayClicked(!overlayClicked);
-    setOpenInfo(false);
+    setOpenSidebar(false);
   };
 
   useEffect(() => {
-    openInfo
+    openSidebar
       ? document.body.classList.add("no-scroll")
       : document.body.classList.remove("no-scroll");
 
     return () => {
       document.body.classList.remove("no-scroll");
     };
-  }, [openInfo]);
+  }, [openSidebar]);
+
   return (
     <>
       <span
         onClick={handleOverlayClick}
         className={`drawer__overlay ${
-          openInfo ? "animate-in overflow-hidden" : ""
+          openSidebar ? "animate-in overflow-hidden" : ""
         } ${overlayClicked ? "animate-out" : ""}`}
       ></span>
       <div
         className={`fixed top-0 ${
-          openInfo ? "right-0" : "-right-[100%]"
-        } ease-in-out text-black-body-color duration-500 transition-all w-full desktop-s:w-4/5 desktop-l:w-3/5 desktop-l:max-w-[870px] h-full overflow-x-hidden overflow-y-auto overscroll-contain z-[999] bg-[#e9e2cd] `}
+          openSidebar ? "right-0" : "-right-[100%]"
+        } ease-in-out text-black-body-color duration-700 desktop-s:duration-1000 transition-all w-full desktop-s:w-4/5 desktop-l:w-3/5 desktop-l:max-w-[870px] h-full overflow-x-hidden overflow-y-auto overscroll-contain z-[999] bg-[#e9e2cd] `}
       >
         <div className="pointer-events-auto relative">
           <div className="px-4 tablet-l:px-10">
@@ -56,98 +59,68 @@ export function TeamInfo({
             <div className="flex flex-col tablet-xl:flex-row tablet-xl:gap-10">
               <div className="tablet-xl:flex-[0_0_40%] tablet-xl:relative">
                 <Image
-                  src="/suit.webp"
+                  src={sidebarInfo.imgHref}
                   width={1280}
                   height={720}
+                  alt={sidebarInfo.imgAlt}
                   className="tablet-xl:sticky tablet-xl:top-32"
                 />
               </div>
 
               <div className="pt-7 flex flex-col gap-5 ">
                 <span className="block font-semibold text-h2-l leading-10">
-                  Rob van de Beeten
+                  {sidebarInfo?.name}
                 </span>
 
                 <div className="flex items-center gap-3">
                   <div className="rounded-full h-[15px] w-[15px] bg-green-900 mb-[2px]"></div>
-                  <span className="block font-medium">CEO & Co-founder</span>
+                  <span className="block font-medium">
+                    {sidebarInfo?.position}
+                  </span>
                 </div>
 
                 <div className="flex flex-col gap-5 tablet-l:gap-7">
                   <div>
                     <span className="block mb-2 text-p-xs">Correo:</span>
-                    <Link href="/">correoprueba123@gmail.com</Link>
+                    <Link href="/">{sidebarInfo.extraInfo?.mail}</Link>
                   </div>
 
                   <div>
                     <span className="block mb-2 text-p-xs">Social:</span>
-
-                    <div className="flex gap-4 tablet-l:gap-7 flex-wrap">
-                      <Link
-                        href="/"
-                        className="bg-red-200 px-3 py-[2px] text-black font-medium w-fit left-4 rounded-[7rem]"
-                      >
-                        LinkedIn
-                      </Link>
-                      <Link
-                        href="/"
-                        className="bg-red-200 px-3 py-[2px] text-black font-medium w-fit left-4 rounded-[7rem]"
-                      >
-                        Instagram
-                      </Link>
-                      <Link
-                        href="/"
-                        className="bg-red-200 px-3 py-[2px] text-black font-medium w-fit left-4 rounded-[7rem]"
-                      >
-                        Instagram
-                      </Link>
-                      <Link
-                        href="/"
-                        className="bg-red-200 px-3 py-[2px] text-black font-medium w-fit left-4 rounded-[7rem]"
-                      >
-                        Instagram
-                      </Link>
+                    <div className="flex gap-4 tablet-l:gap-7 desktop-s:gap-5 flex-wrap">
+                      {sidebarInfo.socials &&
+                        sidebarInfo.socials.map((social) => (
+                          <Link
+                            key={social.key}
+                            href={social.socialHref}
+                            className="bg-red-200 px-3 py-[2px] text-black font-medium w-fit left-4 rounded-[7rem]"
+                          >
+                            {social.socialText}
+                          </Link>
+                        ))}
                     </div>
                   </div>
                 </div>
 
                 <div className="flex flex-col gap-5 tablet-l:gap-7 pb-16">
-                  <p>
-                    Werken voor ondernemers garandeert veel afwisseling. De één
-                    is handelaar, de ander ontwerpt software en weer een derde
-                    produceert halffabricaten voor industriële afnemers. Iedere
-                    cliënt heeft zijn of haar deskundigheid en verwacht die ook
-                    van een advocaat. Die deskundigheid zit hem mijns inziens in
-                    de
-                  </p>
+                  {sidebarInfo.extraInfo?.beforeFunFactText &&
+                    sidebarInfo.extraInfo?.beforeFunFactText.map((p) => (
+                      <p key={p.key}>{p.text}</p>
+                    ))}
 
-                  <div className="">
-                    <span className="font-semibold text-main-heading-color">
-                      Fun fact -{" "}
-                    </span>
-                    <span>Le gusta ir de trekking!</span>
-                  </div>
+                  {sidebarInfo.extraInfo?.funFact && (
+                    <div>
+                      <span className="font-semibold text-main-heading-color">
+                        Fun fact -{" "}
+                      </span>
+                      <span>{sidebarInfo.extraInfo.funFact}</span>
+                    </div>
+                  )}
 
-                  <p>
-                    Ik ben advocaat sinds 1981 en ben gespecialiseerd in
-                    ondernemingsrecht en erfrecht. Ik sta voornamelijk
-                    ondernemers uit het MKB bij, maar werk in erfrechtszaken ook
-                    voor particulieren.
-                  </p>
-
-                  <p>
-                    Ik ben advocaat sinds 1981 en ben gespecialiseerd in
-                    ondernemingsrecht en erfrecht. Ik sta voornamelijk
-                    ondernemers uit het MKB bij, maar werk in erfrechtszaken ook
-                    voor particulieren.
-                  </p>
-
-                  <p>
-                    Ik ben advocaat sinds 1981 en ben gespecialiseerd in
-                    ondernemingsrecht en erfrecht. Ik sta voornamelijk
-                    ondernemers uit het MKB bij, maar werk in erfrechtszaken ook
-                    voor particulieren.
-                  </p>
+                  {sidebarInfo.extraInfo?.afterFunFactText &&
+                    sidebarInfo.extraInfo?.afterFunFactText.map((p) => (
+                      <p key={p.key}>{p.text}</p>
+                    ))}
                 </div>
               </div>
             </div>
@@ -159,11 +132,13 @@ export function TeamInfo({
 }
 
 export default function Team({ teamInfo }) {
-  const [openInfo, setOpenInfo] = useState();
+  const [openSidebar, setOpenSidebar] = useState();
+  const [sidebarInfo, setSidebarInfo] = useState({});
   const [overlayClicked, setOverlayClicked] = useState(false);
 
-  const handleOpenInfo = (name) => {
-    setOpenInfo(name);
+  const handleOpenSidebar = (member) => {
+    setOpenSidebar(true);
+    setSidebarInfo(member);
     setOverlayClicked(false);
   };
 
@@ -186,7 +161,7 @@ export default function Team({ teamInfo }) {
             <div
               key={member.key}
               className="relative cursor-pointer"
-              onClick={() => handleOpenInfo(member.name)}
+              onClick={() => handleOpenSidebar(member)}
             >
               <div>
                 <Image
@@ -194,14 +169,14 @@ export default function Team({ teamInfo }) {
                   width={member.imgWidth}
                   height={member.imgHeight}
                   className="rounded-xl"
-                  onClick={handleOpenInfo}
+                  onClick={() => handleOpenSidebar(member)}
                 />
               </div>
 
               {member.socials &&
                 member.socials.map((social, index) => (
                   <Link
-                    href="/"
+                    href={social.socialHref ? social.socialHref : "/"}
                     key={social.key}
                     className={`absolute bg-white px-3 py-[2px] text-black font-medium ${
                       index === 0 && "top-5"
@@ -225,8 +200,10 @@ export default function Team({ teamInfo }) {
           ))}
       </div>
       <TeamInfo
-        openInfo={openInfo}
-        setOpenInfo={setOpenInfo}
+        openSidebar={openSidebar}
+        setOpenSidebar={setOpenSidebar}
+        sidebarInfo={sidebarInfo}
+        setSidebarInfo={setSidebarInfo}
         overlayClicked={overlayClicked}
         setOverlayClicked={setOverlayClicked}
       />
--- src/data/team.js
+++ src/data/team.js
@@ -18,15 +18,28 @@ export const teamInfo = {
         },
       ],
       extraInfo: {
-        desc: [
-          "Within insolvency law, there are often major interests at stake. Moreover, there is usually pressure on the boiler, which quickly brings matters to a head. In that situation it is important to keep calm, analyze matters quickly and - where possible - take a practical approach.",
-          "I have been a lawyer since 2017 and specialize in insolvency law, commercial disputes and general civil law. I mainly work for SME companies that are in dire straits, want to file for bankruptcy of a counterparty or are unable to collect a claim against a counterparty.",
-          "In addition, I am regularly appointed by the court as a curator in bankruptcies. The knowledge and experience gained in that role also comes in handy in my other businesses.",
+        mail: "correoprueba123@gmail.com",
+        beforeFunFactText: [
+          {
+            text: "Texto1marta Within insolvency law, there are often major interests at stake. Moreover, there is usually pressure on the boiler, which quickly brings matters to a head. In that situation it is important to keep calm, analyze matters quickly and - where possible - take a practical approach.",
+            key: "jejeje-key-example-7",
+          },
+          {
+            text: "Texto2marta I have been a lawyer since 2017 and specialize in insolvency law, commercial disputes and general civil law. I mainly work for SME companies that are in dire straits, want to file for bankruptcy of a counterparty or are unable to collect a claim against a counterparty.",
+            key: "jejeje-key-example-8",
+          },
+          {
+            text: "Texto3marta In addition, I am regularly appointed by the court as a curator in bankruptcies. The knowledge and experience gained in that role also comes in handy in my other businesses.",
+            key: "jejeje-key-example-9",
+          },
         ],
-        interestingFact: "she is a great runner!",
-        factPill: "frequently running in the lausanne marathon",
-        lastParag:
-          "Outside of work, she enjoys traveling, cooking, skiing, hiking, and running, frequently running in the Lausanne Marathon. To recharge, she loves spending time with family, friends, and her dog Luna.",
+        funFact: "she is a great runner!",
+        afterFunFactText: [
+          {
+            text: "Outside of wrk, she enjoys traveling, cooking, skiing, hiking, and running, frequently running in the Lausanne Marathon. To recharge, she loves spending time with family, friends, and her dog Luna.",
+            key: "blablabla-key-example-2",
+          },
+        ],
       },
       key: "example-key-team-1",
     },
@@ -50,12 +63,36 @@ export const teamInfo = {
           key: "social-key-example-2",
         },
       ],
+      extraInfo: {
+        mail: "correoprueba123@gmail.com",
+        beforeFunFactText: [
+          {
+            text: "Texto1brian1 Within insolvency law, there are often major interests at stake. Moreover, there is usually pressure on the boiler, which quickly brings matters to a head. In that situation it is important to keep calm, analyze matters quickly and - where possible - take a practical approach.",
+            key: "jejeje-key-example-6",
+          },
+          {
+            text: "Texto2brian1 I have been a lawyer since 2017 and specialize in insolvency law, commercial disputes and general civil law. I mainly work for SME companies that are in dire straits, want to file for bankruptcy of a counterparty or are unable to collect a claim against a counterparty.",
+            key: "jejeje-key-example-5",
+          },
+          {
+            text: "Texto3brian1 In addition, I am regularly appointed by the court as a curator in bankruptcies. The knowledge and experience gained in that role also comes in handy in my other businesses.",
+            key: "jejeje-key-example-4",
+          },
+        ],
+        funFact: "she is a great boxer!",
+        afterFunFactText: [
+          {
+            text: "Outside of wok, she enjoys traveling, cooking, skiing, hiking, and running, frequently running in the Lausanne Marathon. To recharge, she loves spending time with family, friends, and her dog Luna.",
+            key: "blablabla-key-example-3",
+          },
+        ],
+      },
       key: "example-key-team-2",
     },
 
     {
       name: "Brian Herbert",
-      position: "Co-Founder, CEO",
+      position: "UX Designer",
       imgHref: "/miembro3.jpg",
       imgAlt: "",
       imgWidth: 1280,
@@ -67,6 +104,30 @@ export const teamInfo = {
           key: "social-key-example-4",
         },
       ],
+      extraInfo: {
+        mail: "correoprueba123@gmail.com",
+        beforeFunFactText: [
+          {
+            text: "Texto1brian2 Within insolvency law, there are often major interests at stake. Moreover, there is usually pressure on the boiler, which quickly brings matters to a head. In that situation it is important to keep calm, analyze matters quickly and - where possible - take a practical approach.",
+            key: "jejeje-key-example-1",
+          },
+          {
+            text: "Texto2brian2 I have been a lawyer since 2017 and specialize in insolvency law, commercial disputes and general civil law. I mainly work for SME companies that are in dire straits, want to file for bankruptcy of a counterparty or are unable to collect a claim against a counterparty.",
+            key: "jejeje-key-example-2",
+          },
+          {
+            text: "Texto3brian2 In addition, I am regularly appointed by the court as a curator in bankruptcies. The knowledge and experience gained in that role also comes in handy in my other businesses.",
+            key: "jejeje-key-example-3",
+          },
+        ],
+        funFact: "she is a great gymnast!",
+        afterFunFactText: [
+          {
+            text: "Outside of ork, she enjoys traveling, cooking, skiing, hiking, and running, frequently running in the Lausanne Marathon. To recharge, she loves spending time with family, friends, and her dog Luna.",
+            key: "blablabla-key-example-1",
+          },
+        ],
+      },
       key: "example-key-team-3",
     },
   ],

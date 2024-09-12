67d6b478f3da13247f5d33ea78a4348cf5af5cf2
diego orellana <diego.orellana.git@gmail.com>
2024-09-12 14:06:07 -0300

    solved minor bug with pop-up

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -75,14 +75,14 @@ export const Header = () => {
       ></span>
       <div
         className={`${
-          openedForm && "translate-y-0 opacity-100"
-        } translate-y-[120%] z-[999] pointer-events-none opacity-0 ease-in-out duration-700 transition-all fixed h-full -mt-[96px] w-full flex items-center`}
+          openedForm ? "translate-y-0 opacity-100" : "translate-y-[120%]"
+        } z-[999] pointer-events-none opacity-0 ease-in-out duration-700 transition-all fixed h-full -mt-[96px] w-full flex items-center`}
       >
         <div className="rounded-xl pointer-events-auto mx-[12%] h-fit flex bg-white w-full">
           <div className="relative hidden desktop-s:block w-full tablet-xl:flex-[0_0_40%] desktop-l:flex-[0_0_45%]">
             <Image
               src={colegiosFormInfo.imgHref}
-              className="rounded-l-xl w-full mb-7 tablet-s:mb-12 tablet-xl:mb-0  object-cover max-h-96 tablet-l:max-h-full tablet-l:h-[500px] tablet-xl:h-full"
+              className="rounded-l-xl w-full mb-7 tablet-s:mb-12 tablet-xl:mb-0 object-cover max-h-96 tablet-l:max-h-full tablet-l:h-[500px] tablet-xl:h-full"
               width={613}
               height={1313}
               alt={colegiosFormInfo.imgAlt}
